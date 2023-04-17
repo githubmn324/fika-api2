c76b2b7d297dee51003b75ae30e1d8e23d9d307b
scsk-nakagome <nakagome@scsk.jp>
2023-04-17 18:29:51 +0900

    4/17

--- main.go
+++ main.go
@@ -4,31 +4,30 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
+	"net/http/httputil"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	log.Print("starting server...")
-	http.HandleFunc("/", handler)
 
-	// Determine port for HTTP service.
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-		log.Printf("defaulting to port %s", port)
-	}
+	log.Printf("starting HTTP")
 
-	// Start HTTP server.
-	log.Printf("listening on port %s", port)
-	if err := http.ListenAndServe(":"+port, nil); err != nil {
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		requestDump, err := httputil.DumpRequest(c.Request, true)
+		if err != nil {
+			fmt.Println(err)
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"message":      "hello I'm Cloud Run API 2!",
+			"requstHeader": string(requestDump),
+		})
+	})
+
+	if err := r.Run(":8080"); err != nil {
 		log.Fatal(err)
 	}
-}
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	name := os.Getenv("NAME")
-	if name == "" {
-		name = "Cloud Run 2"
-	}
-	fmt.Fprintf(w, "Hello %s!\n", name)
 }
